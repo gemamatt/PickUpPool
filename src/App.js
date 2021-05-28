cf276ef41762936082e7da6cdb2e801cef7cff63
MichelleFragosoC <michellefragosoc@outlook.com>
2021-05-28 03:21:59 -0500

    Funcionalidad de login y register

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import {auth} from './firebase';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserOrders from './components/Orders/UserOrders';
@@ -7,13 +8,24 @@ import { Register } from './components/Register/Register';
 import { Login } from './components/Login/Login';
 
 function App() {
-
+  const [user, setUser] = useState(null) //No hubo nada establecido(ningún valoor)
+  useEffect(() => { //Estado del servidor
+    auth.onAuthStateChanged((user) => { //user del servidor
+      if (user) {
+        setUser(user) //Cuando existe el usuario setUser. Sincro
+      } else {
+        setUser(false) //Cuando no existe el usuario
+      }
+    })
+  }, [])
   return (
+    <>
+    {user !== null ? (
     <Router>
       <div className="App">
         <Switch>
           <Route path='/principal'>
-            <Principal/>
+            <Principal />
           </Route>
           <Route path='/register'>
             <Register />
@@ -22,12 +34,13 @@ function App() {
             <UserOrders />
           </Route>
           <Route exact path='/'>
-            <Login />
+            <Login user={user} />
           </Route>
         </Switch>
       </div>
     </Router>
+    ): <p>Cargando...</p>}
+    </>
   );
 }
-
 export default App;
