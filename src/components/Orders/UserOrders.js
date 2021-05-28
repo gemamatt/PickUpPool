448f55806fed9c3df090212ab463d38384cbdee3
unknown <danycorreos96@gmail.com>
2021-05-28 15:59:22 -0500

    ordenes de pedidos usuario

--- src/components/Orders/UserOrders.js
+++ src/components/Orders/UserOrders.js
@@ -4,19 +4,16 @@ import OneOrder from './OneOrder'
 import './UserOrders.css'
 
 function UserOrders() {
+    
+
     return (
         <>
             <Navbar />
             <div className='conteiner-userorders'>
-                <div className="my-orders">
-                    <h1>Mis pedidos</h1>
-                </div>
-                <section>
+                <h1>Mis pedidos</h1>
                     <div className="conteiner-orders">
                         <OneOrder />
-                        <OneOrder />
                     </div>
-                </section>
             </div>
         </>
 
