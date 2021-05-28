4322458229c33c60eee8edc4052b88e2dfaf02d4
MichelleFragosoC <michellefragosoc@outlook.com>
2021-05-28 02:22:43 -0500

    maquetación de vista login/register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserOrders from './components/Orders/UserOrders';
 import Principal from './components/Principal/Principal';
+import { Register } from './components/Register/Register';
+import { Login } from './components/Login/Login';
 
 function App() {
 
@@ -14,13 +16,13 @@ function App() {
             <Principal/>
           </Route>
           <Route path='/register'>
-            
+            <Register />
           </Route>
           <Route path='/orders'>
             <UserOrders />
           </Route>
           <Route exact path='/'>
-            
+            <Login />
           </Route>
         </Switch>
         </div>
