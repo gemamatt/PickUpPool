e13da6df02eaaa575e9f52914fad71d77f1f830b
Karly-G <gonzalezcervantes.karla@gmail.com>
2021-05-27 22:12:14 -0500

    navbar & principal view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Principal from './components/Principal/Principal'
 
 function App() {
   return (
     <div className="App">
-      <h1>Pick Up Pool</h1>
+      <Principal/>
     </div>
   );
 }
