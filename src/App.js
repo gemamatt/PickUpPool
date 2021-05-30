68e6f6c932dd209e66d2748e82d9204f15705c79
MichelleFragosoC <michellefragosoc@outlook.com>
2021-05-29 20:27:32 -0500

    Funcionalidad de la pantalla de entregas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import {auth} from './firebase';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserOrders from './components/Orders/UserOrders';
+import PickerOrders from './components/Orders/PickerOrders';
 import Principal from './components/Principal/Principal';
+import DetailOrder from './components/Orders/DetailOrder'
 import { Register } from './components/Register/Register';
 import { Login } from './components/Login/Login';
 
@@ -33,8 +35,16 @@ function App() {
           <Route path='/orders'>
             <UserOrders user={user}/>
           </Route>
+
+          <Route path='/deliveries'>
+            <PickerOrders user={user}/>
+          </Route>
+
           <Route exact path='/'>
             <Login user={user} />
+          </Route>
+          <Route path='/details'>
+            <DetailOrder />
           </Route>
         </Switch>
       </div>
