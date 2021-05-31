8afc448dcdc7109b2a1d53fd64bc954ac60b3c5e
Gema Matt <61843604+gemamatt@users.noreply.github.com>
2021-05-31 15:31:27 -0500

    codigo unificado y actualizado

--- src/components/OrderTracking/MapView.js
+++ src/components/OrderTracking/MapView.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
  
-
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 const myLatLng = {lat: 19.4368747, lng: -99.1412259};
  
 class ViewMaps extends Component {
   static defaultProps = {
     center: myLatLng,
-    zoom: 11
+    zoom: 12
   };
  
   render() {
@@ -17,7 +17,18 @@ class ViewMaps extends Component {
           bootstrapURLKeys={{ key: 'AIzaSyC6ymkUmHMLmj5Sr6xDHhC50YYfmT1QP3U' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-        />
+        >
+          <AnyReactComponent
+            lat={19.43676970998405}
+            lng={-99.14099943570316}
+            text="Usuario"
+          />
+           <AnyReactComponent
+            lat={19.372794328102774}
+            lng={-99.17858404330478}
+            text="piker"
+          />
+        </GoogleMapReact>
       </div>
     );
   }
--- src/components/OrderTracking/OrderTracking.js
+++ src/components/OrderTracking/OrderTracking.js
@@ -1,36 +1,10 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import './OrderTracking.css'
 import MapView from './MapView';
-import OrderLocation from './OrderLocation'
-import GoogleMapReact from 'google-map-react';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function OrderTracking() {
-    const [lat, setLat] = useState (19.43676970998405);
-    const [lng, setLng] = useState (-99.14099943570316);
-
-    const setCenter = (lat,lng) =>{
-        setLat (lat);
-        setLng (lng);
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyC6ymkUmHMLmj5Sr6xDHhC50YYfmT1QP3U' }}
-          defaultCenter={setLat,setLng}
-          defaultZoom={12}
-        />
-    };
-
-    const addMarker = (lat, lng) => {
-        return (
-            <AnyReactComponent
-            latitud={lat}
-            longitud={lng}
-            text = "My Marker"
-          />
-        );
-      };
-   return (
+    return (
         <>
             <Navbar />
             <div className='conteiner-orderTracking'>
@@ -39,8 +13,7 @@ function OrderTracking() {
                 </div>
                 <section>
                     <div className="conteiner-map">
-                       <MapView OrderLocation  setCenter={setCenter} setMarker={addMarker}/>
-                       
+                       <MapView />
                     </div>
                 </section>
             </div>
